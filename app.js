da0a671ccad38fea4bfd3181a166d34e9816936e
Fadomire <fadomire@gmail.Com>
2013-01-23 23:34:47 +0100

    DB change to comply with heroku and mongolab

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require("express"),
     request = require("request"),
     io = require('socket.io'),
-    mongodb = require('mongodb'),
-    server = new mongodb.Server('localhost', 27017, {auto_reconnect: true}),
-    db = new mongodb.Db('futureAuth', server, {safe:true}),
-    port = process.env.PORT || 8080;
+    mongodb = require('mongodb');
 
-db.open(function(e, db) {})
-var Users = db.collection('users');
+var port = process.env.PORT || 8080,
+    dbUri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/futureAuth";
+
 
 var faceKey = "1e9a6adc96ca4f8b845d246351e4d25b";
 var faceSecret = "23515e6100d94cbbb9a44f68b3874707";
@@ -22,107 +20,116 @@ io.sockets.on('connection', function (socket) {
   socket.on('login', function (image) {
     var imageBase = image.replace(/^data:image\/\w+;base64,/, "");
     var buf = new Buffer(imageBase, 'base64');
-
-    Users.distinct("name",function (err, items){
-      var usersCollection = items.join(",");
-      request({
-      url: 'http://api.skybiometry.com/fc/faces/recognize.json',
-      headers: {
-          'content-type' : 'multipart/form-data'
-      },
-      method: 'POST',
-      multipart: [{ 
-          'Content-Disposition' : 'form-data; name="file"; filename="temp.jpg"',
-          'Content-Type' : 'image/jpeg',
-          body: buf
-      },{ 
-          'Content-Disposition' : 'form-data; name="api_key";',
-          body: faceKey
-      },{ 
-          'Content-Disposition' : 'form-data; name="api_secret";',
-          body: faceSecret
-      },{ 
-          'Content-Disposition' : 'form-data; name="attributes";',
-          body: "all"
-      },{ 
-          'Content-Disposition' : 'form-data; name="detector";',
-          body: "Agressive"
-      },{ 
-          'Content-Disposition' : 'form-data; name="uids";',
-          body: usersCollection
-      },{ 
-          'Content-Disposition' : 'form-data; name="namespace";',
-          body: "futureAuth"
-      }]
-      }, 
-      function(err, res, body){
-        socket.emit("loginCallback", body);
+    mongodb.MongoClient.connect(dbUri, {auto_reconnect: true}, function (error, db){
+      var Users = db.collection("users")
+      Users.distinct("name",function (err, items){
+        db.close();
+        var usersCollection = items.join(",");
+        request({
+          url: 'http://api.skybiometry.com/fc/faces/recognize.json',
+          headers: {
+              'content-type' : 'multipart/form-data'
+          },
+          method: 'POST',
+          multipart: [{ 
+              'Content-Disposition' : 'form-data; name="file"; filename="temp.jpg"',
+              'Content-Type' : 'image/jpeg',
+              body: buf
+          },{ 
+              'Content-Disposition' : 'form-data; name="api_key";',
+              body: faceKey
+          },{ 
+              'Content-Disposition' : 'form-data; name="api_secret";',
+              body: faceSecret
+          },{ 
+              'Content-Disposition' : 'form-data; name="attributes";',
+              body: "all"
+          },{ 
+              'Content-Disposition' : 'form-data; name="detector";',
+              body: "Agressive"
+          },{ 
+              'Content-Disposition' : 'form-data; name="uids";',
+              body: usersCollection
+          },{ 
+              'Content-Disposition' : 'form-data; name="namespace";',
+              body: "futureAuth"
+          }]
+        }, 
+        function(err, res, body){
+          socket.emit("loginCallback", body);
+        });
       });
-    });
+    })
+
 });
   socket.on('signup', function (data) {
-    Users.findOne({name: data.username}, function(err, item) {
-      if(item){
-        socket.emit("signupFailCallback", item.name);
-      }else{
-        Users.insert({name: data.username}, function (err, inserted) {
-          if(err){
-            console.log("There was a problem savin;g this user"+err);
-          } else {
-            console.log("User inserted"+inserted);
-            var imageBase = data.image.replace(/^data:image\/\w+;base64,/, "");
-            var buf = new Buffer(imageBase, 'base64');
-            request({
-              url: 'http://api.skybiometry.com/fc/faces/detect.json',
-              method: 'POST',
-              multipart: [{ 
-                  'Content-Disposition' : 'form-data; name="file"; filename="temp.jpg"',
-                  'Content-Type' : 'image/jpeg',
-                  body: buf
-              },{ 
-                  'Content-Disposition' : 'form-data; name="api_key";',
-                  body: faceKey
-              },{ 
-                  'Content-Disposition' : 'form-data; name="api_secret";',
-                  body: faceSecret
-              },{ 
-                  'Content-Disposition' : 'form-data; name="attributes";',
-                  body: "all"
-              },{ 
-                  'Content-Disposition' : 'form-data; name="detector";',
-                  body: "Agressive"
-              }]
-            }, 
-            function(err, res, body){
-              var response = JSON.parse(body);
-              // save the tag to API database
-              try{
-                if(response.photos[0].tags[0].tid){
-                  request('http://api.skybiometry.com/fc/tags/save.json?api_key='+faceKey
-                    +'&api_secret='+faceSecret
-                    +'&uid='+data.username
-                    +'@futureAuth&tids='+response.photos[0].tags[0].tid, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                      request('http://api.skybiometry.com/fc/faces/train.json?api_key='+faceKey
-                        +'&api_secret='+faceSecret
-                        +'&uids='+data.username+'@futureAuth'
-                        , function (error, response, body) {
-                          if (!error && response.statusCode == 200) {
-                            socket.emit("signupSuccessCallback", body);
-                          }
-                      })
-                    }
-                  })
+    mongodb.MongoClient.connect(dbUri, {auto_reconnect: true}, function (error, db){
+      var Users = db.collection("users");
+      Users.findOne({name: data.username}, function (err, item) {
+        if(item){
+          socket.emit("signupFailCallback", item.name);
+        }else{
+          Users.insert({name: data.username}, function (err, inserted) {
+            db.close();
+            if(err){
+              console.log("There was a problem savin;g this user"+err);
+            } else {
+              console.log("User inserted"+inserted);
+              var imageBase = data.image.replace(/^data:image\/\w+;base64,/, "");
+              var buf = new Buffer(imageBase, 'base64');
+              request({
+                url: 'http://api.skybiometry.com/fc/faces/detect.json',
+                method: 'POST',
+                multipart: [{ 
+                    'Content-Disposition' : 'form-data; name="file"; filename="temp.jpg"',
+                    'Content-Type' : 'image/jpeg',
+                    body: buf
+                },{ 
+                    'Content-Disposition' : 'form-data; name="api_key";',
+                    body: faceKey
+                },{ 
+                    'Content-Disposition' : 'form-data; name="api_secret";',
+                    body: faceSecret
+                },{ 
+                    'Content-Disposition' : 'form-data; name="attributes";',
+                    body: "all"
+                },{ 
+                    'Content-Disposition' : 'form-data; name="detector";',
+                    body: "Agressive"
+                }]
+              }, 
+              function(err, res, body){
+                var response = JSON.parse(body);
+                // save the tag to API database
+                try{
+                  if(response.photos[0].tags[0].tid){
+                    request('http://api.skybiometry.com/fc/tags/save.json?api_key='+faceKey
+                      +'&api_secret='+faceSecret
+                      +'&uid='+data.username
+                      +'@futureAuth&tids='+response.photos[0].tags[0].tid, function (error, response, body) {
+                      if (!error && response.statusCode == 200) {
+                        request('http://api.skybiometry.com/fc/faces/train.json?api_key='+faceKey
+                          +'&api_secret='+faceSecret
+                          +'&uids='+data.username+'@futureAuth'
+                          , function (error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                              socket.emit("signupSuccessCallback", body);
+                            }
+                        })
+                      }
+                    })
+                  }
+                }catch(e){
+                  
                 }
-              }catch(e){
                 
-              }
-              
-            });
-          }
-        });
-      }
-    });
+              });
+            }
+          });
+        }
+      });
+    })
+
   });
 });
 app.set('view engine', 'ejs');
