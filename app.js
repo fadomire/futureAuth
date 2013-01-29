f56bd76963621bd3138b679f4157002fcf6bf1c1
Fadomire <fadomire@gmail.Com>
2013-01-29 13:14:24 +0100

    ios compatibility

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ var app = express()
   , server = require('http').createServer(app)
   , io = io.listen(server);
 
-io.configure(function(){
-  io.set("transports", ["xhr-polling"])
-  io.set("polling duration", 10)
-})
+// io.configure(function(){
+//   io.set("transports", ["xhr-polling"])
+//   io.set("polling duration", 10)
+// })
 
 server.listen(port);
 
@@ -30,9 +30,10 @@ io.sockets.on('connection', function (socket) {
     mongodb.MongoClient.connect(dbUri, {auto_reconnect: true}, function (error, db){
       console.log(error);
       var Users = db.collection("users")
-      Users.distinct("name",function (err, items){
+      Users.distinct("_id",function (err, items){
         db.close();
         var usersCollection = items.join(",");
+        console.log(usersCollection)
         request({
           url: 'http://api.skybiometry.com/fc/faces/recognize.json',
           headers: {
@@ -71,68 +72,81 @@ io.sockets.on('connection', function (socket) {
 
 });
   socket.on('signup', function (data) {
+    console.log(data.username)
     mongodb.MongoClient.connect(dbUri, {auto_reconnect: true}, function (error, db){
       var Users = db.collection("users");
       Users.findOne({name: data.username}, function (err, item) {
         if(item){
-          socket.emit("signupFailCallback", item.name);
+          socket.emit("signupFailCallback", "Sorry username "+item.name+" is already taken");
         }else{
-          Users.insert({name: data.username}, function (err, inserted) {
-            db.close();
-            if(err){
-              console.log("There was a problem savin;g this user"+err);
-            } else {
-              console.log("User inserted"+inserted);
-              var imageBase = data.image.replace(/^data:image\/\w+;base64,/, "");
-              var buf = new Buffer(imageBase, 'base64');
-              request({
-                url: 'http://api.skybiometry.com/fc/faces/detect.json',
-                method: 'POST',
-                multipart: [{ 
-                    'Content-Disposition' : 'form-data; name="file"; filename="temp.jpg"',
-                    'Content-Type' : 'image/jpeg',
-                    body: buf
-                },{ 
-                    'Content-Disposition' : 'form-data; name="api_key";',
-                    body: faceKey
-                },{ 
-                    'Content-Disposition' : 'form-data; name="api_secret";',
-                    body: faceSecret
-                },{ 
-                    'Content-Disposition' : 'form-data; name="attributes";',
-                    body: "all"
-                },{ 
-                    'Content-Disposition' : 'form-data; name="detector";',
-                    body: "Agressive"
-                }]
-              }, 
-              function(err, res, body){
-                var response = JSON.parse(body);
-                // save the tag to API database
-                try{
-                  if(response.photos[0].tags[0].tid){
+          var imageBase = data.image.replace(/^data:image\/\w+;base64,/, "");
+          var buf = new Buffer(imageBase, 'base64');
+          request({
+            url: 'http://api.skybiometry.com/fc/faces/detect.json',
+            method: 'POST',
+            multipart: [{ 
+                'Content-Disposition' : 'form-data; name="file"; filename="temp.jpg"',
+                'Content-Type' : 'image/jpeg',
+                body: buf
+            },{ 
+                'Content-Disposition' : 'form-data; name="api_key";',
+                body: faceKey
+            },{ 
+                'Content-Disposition' : 'form-data; name="api_secret";',
+                body: faceSecret
+            },{ 
+                'Content-Disposition' : 'form-data; name="attributes";',
+                body: "all"
+            },{ 
+                'Content-Disposition' : 'form-data; name="detector";',
+                body: "Agressive"
+            }]
+          }, 
+          function (err, res, body){
+            console.log(body)
+            var jsonResponse = JSON.parse(body);
+            // save the tag to API database
+            try{
+              if(jsonResponse.photos[0].tags[0].tid){
+                Users.insert({name: data.username}, function (err, inserted) {
+                  db.close();
+                  if(err){
+                    console.log("There was a problem saving this user"+err);
+                  } else {
+                    console.log("User inserted"+inserted[0]._id);
                     request('http://api.skybiometry.com/fc/tags/save.json?api_key='+faceKey
                       +'&api_secret='+faceSecret
-                      +'&uid='+data.username
-                      +'@futureAuth&tids='+response.photos[0].tags[0].tid, function (error, response, body) {
+                      +'&uid='+inserted[0]._id
+                      +'@futureAuth&tids='+jsonResponse.photos[0].tags[0].tid
+                      +'&label='+data.username
+                      , function (error, response, body) {
+                        console.log(body)
+                        var jsonResponse = JSON.parse(body);
                       if (!error && response.statusCode == 200) {
                         request('http://api.skybiometry.com/fc/faces/train.json?api_key='+faceKey
                           +'&api_secret='+faceSecret
-                          +'&uids='+data.username+'@futureAuth'
+                          +'&uids='+inserted[0]._id+'@futureAuth'
                           , function (error, response, body) {
+                            console.log(body)
+                            var jsonResponse = JSON.parse(body);
                             if (!error && response.statusCode == 200) {
                               socket.emit("signupSuccessCallback", body);
+                            }else{
+                              socket.emit("signupFailCallback", "There was a problem with the request "+jsonResponse.error_message);
+
                             }
                         })
+                      }else{
+                        socket.emit("signupFailCallback", "There was a problem with the request "+jsonResponse.error_message);
                       }
                     })
                   }
-                }catch(e){
-                  
-                }
-                
-              });
+                })
+              }
+            }catch(e){
+              socket.emit("signupFailCallback", "Sorry your face was not detected properly, please retry");
             }
+            
           });
         }
       });
--- public/js/client.js
+++ public/js/client.js
@@ -1,27 +1,51 @@
 function init() {
   var socket = io.connect('http://'+window.location.hostname);
+  
+  function alertHandling(type, message){
+    $('#alert_placeholder').html('<div class="alert '+type+'"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+  }
+  //console.log(socket)
   socket.on('loginCallback', function (data) {
     // convert json response to object
-    var response = JSON.parse(data);
-    handleResult(response.photos[0]);
+    var jsonResponse = JSON.parse(data);
+    console.log(jsonResponse)
+    try{
+      handleResult(jsonResponse.photos[0]);
+    }catch(e){
+      alertHandling('alert-error', 'Sorry you were not recognized')
+    }
   });
   socket.on('signupSuccessCallback', function (data) {
     alertHandling('alert-success', 'Your signed up, you can now login');
   });
   socket.on('signupFailCallback', function (data) {
-    alertHandling('alert-error', 'Sorry username '+data+' is already taken');
+    alertHandling('alert-error', data);
   });
 
   window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
   navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
   navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-  if (navigator.getUserMedia) {
+  setTimeout(function () {
+    if (navigator.getUserMedia) {
     var video = document.getElementById('monitor');
     var canvas = document.getElementById('photo');
+    if( ! window.URL ){
+      window.URL={};
+    }
+    if( ! window.URL.createObjectURL) {
+      window.URL.createObjectURL=function(obj){return obj;}
+    }
+
     navigator.getUserMedia({video:true},
       function successCallback(stream) {
         video.src = window.URL.createObjectURL(stream) || stream;
+        try {
+          var pc = new PeerConnection();
+          pc.addStream(stream);
+        }catch(e){
+
+        }
+        
         video.play();
         
         $("#snapshotbutton").click(snapshot);
@@ -33,40 +57,75 @@ function init() {
 
     function snapshot() {
       alertHandling('alert-info', 'Authentication ongoing...');
-
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      canvas.getContext('2d').drawImage(video, 0, 0);
-
-      var dataUrl = canvas.toDataURL('image/jpeg', 1.0);
-      socket.emit("login",dataUrl);
+      
+      if($("img").length>3){
+        var snapshot = document.getElementsByTagName("img")[3].cloneNode(true);
+        
+        setTimeout(function () {
+          canvas.width = snapshot.width;
+          canvas.height = snapshot.height;
+          canvas.getContext('2d').drawImage(snapshot, 0, 0);
+          var dataUrl = canvas.toDataURL('image/jpeg', 1.0);
+          console.log(dataUrl);
+          socket.emit("login",dataUrl);
+        }, 500);
+      }else{
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d').drawImage(video, 0, 0);
+        var dataUrl = canvas.toDataURL('image/jpeg', 1.0);
+        console.log(dataUrl);
+        socket.emit("login",dataUrl);
+      }
+      
+      
+      
 
       
     }
 
     function signup() {
+      
       alertHandling('alert-info', 'Registration ongoing..');
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      canvas.getContext('2d').drawImage(video, 0, 0);
-
-      var dataUrl = canvas.toDataURL('image/jpeg', 1.0);
       var username = $("#username").val();
-      if(username!=""){
-        socket.emit("signup",{
-          username: username,
-          image: dataUrl
-        });
+      if($("img").length>3){
+        var snapshot = document.getElementsByTagName("img")[3].cloneNode(true);
+        
+        setTimeout(function () {
+          canvas.width = snapshot.width;
+          canvas.height = snapshot.height;
+          canvas.getContext('2d').drawImage(snapshot, 0, 0);
+          var dataUrl = canvas.toDataURL('image/jpeg', 1.0);
+          if(username!=""){
+            socket.emit("signup",{
+              username: username,
+              image: dataUrl
+            });
+          }else{
+            alertHandling('alert-error', 'Please enter a name to signup');
+          } 
+        }, 500);
       }else{
-        alertHandling('alert-error', 'Please enter a name to signup');
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d').drawImage(video, 0, 0);
+        var dataUrl = canvas.toDataURL('image/jpeg', 1.0);
+        if(username!=""){
+          socket.emit("signup",{
+            username: username,
+            image: dataUrl
+          });
+        }else{
+          alertHandling('alert-error', 'Please enter a name to signup');
+        }
       }
     }
 
 
     function handleResult(photo) {
       try{
-        if(photo.tags[0].uids[0].confidence > 50){
-          alertHandling('alert-success', 'Welcome '+photo.tags[0].uids[0].uid.split('@')[0]+' !');
+        if(photo.tags[0].uids[0].confidence > 40){
+          alertHandling('alert-success', 'Welcome '+photo.tags[0].label+' !');
           $("#app").html("");
         }else{
           alertHandling('alert-error', 'Sorry you were not recognized');
@@ -77,13 +136,15 @@ function init() {
       
     }
 
-    function alertHandling(type, message){
-      $('#alert_placeholder').html('<div class="alert '+type+'"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
-    }
+    
 
   } else {
     alertHandling('alert-error', 'No native camera support available.');
   }
+}, 500);
+  
 
 }
-init();
+$(document).ready(function() {
+  init();
+});
